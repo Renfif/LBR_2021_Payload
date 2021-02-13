246c9c77ac94770ff29a6976bafbeb787599e089
T3chnopath <76810853+T3chnopath@users.noreply.github.com>
2021-02-12 16:59:36 -0800

    PJS Fullscale Draft

--- PJS_Firmware/PJS_Fullscale/PJS_Fullscale.ino
+++ PJS_Firmware/PJS_Fullscale/PJS_Fullscale.ino
@@ -1,13 +1,17 @@
+//https://create.arduino.cc/projecthub/electropeak/sd-card-module-with-arduino-how-to-read-write-data-37f390
+
+#include <PJS_FullscaleFunctions.h>
 #include <Wire.h> 
 #include <Adafruit_Sensor.h>
 #include "Adafruit_BMP3XX.h"
 
 
 //Switch pins
-#define Switch1_Link      // (x1) when clicked the linkage folded, when unclicked the linkage is extended
-#define Switch2_Link      // (x1) when clicked the linkage is unfolded, when unclicked the linkage is not extended
-#define Switch3_DoorLock  // (x2) when the limit switch is clicked that means door is unlocked (though cannot do a check for locking)
-#define Switch4_UAS       // (x1) when unclicked PJS is deployed, when clicked PJS is inserted
+#define Switch1_Link __      // (x1) when clicked the linkage folded, when unclicked the linkage is extended
+#define Switch2_Link __      // (x1) when clicked the linkage is unfolded, when unclicked the linkage is not extended
+#define Switch3_DoorLock __  // (x2) when the limit switch is clicked that means door is unlocked (though cannot do a check for locking)
+#define Switch4_UAS __       // (x1) when unclicked PJS is deployed, when clicked PJS is inserted
+
 
 //Driver pins
 #define UAS_DriverIN1 0 
@@ -20,6 +24,7 @@
 #define LockB_DriverIN2 7
 #define Relay_Signal 12
 
+
 //RF Receiver pins
 #define RF_CH1 8
 #define RF_CH2 9
@@ -27,6 +32,7 @@
 #define RF_CH4 11
 #define RF_CH5 13
 #define RF_CH6 14
+
 
 //Altimeter (I2C)
 #define SEALEVELPRESSURE_HPA (1013.25) //adjustment for pressure
@@ -64,17 +70,18 @@ void setup() {
   pinMode(LockB_DriverIN2, OUTPUT);
 }
 
+
 void loop() {
 
   //getting altitude and velocity
-  altitude = bmp.readAltitude(SEALEVELPRESSURE_HPA);
+  float altitude = bmp.readAltitude(SEALEVELPRESSURE_HPA);
   delay(10); 
-  altitudeNew = bmp.readAltitude(SEALEVELPRESSURE_HPA);
+  float altitudeNew = bmp.readAltitude(SEALEVELPRESSURE_HPA);
 
   dy = altitudeNew - altitude
   dt = 0.01 
 
-  velocity = dy/dt 
+  velocity = dy/dt //what units does this come out as?
 
   //get stuff from receiver. Only getting CH2 - CH6 input during installation and setup
   Ch1 = receive(receiver1) //safety switch
@@ -88,7 +95,7 @@ void loop() {
 
   if(Ch1 > 200) { 
     
-    if(Ch2 > 200)&&(altitude > 550)&&(altitude < 650)&&(velocity < 30)&& { 
+    if(Ch2 > 200)&&(altitude > 550)&&(altitude < 650)&&(velocity < 30){ 
       deployment(Time, switch1_Link, switch2_Link, switch3_DoorLock, switch4_UAS)
     }
     
@@ -122,128 +129,4 @@ void loop() {
     
   }
   
-}
-
-
-int receive(receiverX) { 
-  
-  int Ch=pulseIn(receiverX,HIGH,25000);
-  
-  Ch=pulseToPWM(Ch);
-
-  return Ch;
-
-}
-
-
-int pulseToPWM(int pulse){
-  if (pulse>1000){
-    pulse = map(pulse, 1000, 2000, -500, 500);
-    pulse = constrain(pulse, -255, 255);  
-  }else{
-    pulse=0;  
-  }
-  if(abs(pulse)<= deadzone){
-    pulse=0;
-  }
-  return pulse;
-}
-
-
-void deployment(Time, switch1_Link, switch2_Link, switch3_DoorLock, switch4_UAS) { 
-
-  //if Switch3s are clicked (door unlocked), open door
-  while(!Switch3_DoorLock == HIGH) { 
-    unlockDoor(); 
-  }
-  
-
-  
-  if(Switch3_DoorLock == HIGH) { 
-    openDoor(); //need to do an until check to stop the motor?
-  }
-
-  //if Switch1_Link is unclicked and Switch2_Link is clicked (door open), deploy UAS
-  if(Switch1_Link == LOW)&&(Switch2_Link == HIGH){ 
-    deployUAS();
-  }
-
-  //if Switch4_UAS is low (UAS deployed), close door
-  if(switch4_UAS == LOW) {
-    closeDoor();
-  }
-
-  //if Switch1_Link is clicked and Switch2_LInk is unclocked (door closed), lock door
-  if(Switch1_Link == HIGH)&&(Switch2_Link == LOW) { 
-    lockDoor()
-    }
- 
-}
-
-
-//if we do ramping, wont it ramp up and down
-
-void unlockDoor() {
-  for(int i = 255; i < 255; i++) { 
-    analogWrite(LockA_DriverIN1, i); 
-    analogWrite(LockA_DriverIN2, 0);
-    delay(5); 
-  }
-  while(1) { 
-    analogWrite(LockA_DriverIN1, 255); 
-    analogWrite(LockA_DriverIN2, 0);
-  }
-}
-
-
-void lockDoor() { 
-  for(int i = 255; i < 255; i++) { 
-    analogWrite(LockA_DriverIN2, i); 
-    analogWrite(LockA_DriverIN1, 0);
-    delay(10); 
-  }
-}
-
-
-void openDoor() { 
-  for(int i = 255; i < 255; i++) { 
-    analogWrite(LinkDriverIN1, i); 
-    analogWrite(LinkDriverIN2, 0); 
-    delay(10); 
-  }
-}
-
-
-void closeDoor() { //ramp down 
-  for(int i = 255; i < 255; i++) { 
-    analogWrite(LinkDriverIN2, i); 
-    analogWrite(LinkDriverIN1, 0); 
-    delay(10); 
-  }
-}
-
-
-void deployUAS() { 
-  for(int i = 255; i < 255; i++) { 
-    analogWrite(UAS_DriverIN1, i); 
-    analogWrite(UAS_DriverIN2, 0); 
-    delay(10); 
-  }
-}
-
-
-void installUAS() { 
-  for(int i = 255; i < 255; i++) { 
-    analogWrite(UAS_DriverIN2, i); 
-    analogWrite(UAS_DriverIN1, 0); 
-    delay(10); 
-  }
-}
-//should we do this nested fuction? 
-int motor(int INA, int INB) { 
-  for(int i = 255; i < 255; i) { 
-    analogWrite(INA, i); 
-    analogWrite(INB, 0); 
-    delay(10); 
-  }
 }
---
+++ PJS_Firmware/PJS_Fullscale/PJS_FullscaleFunctions.h
@@ -0,0 +1,170 @@
+
+
+int receive(receiverX) { 
+  int Ch=pulseIn(receiverX,HIGH,25000);
+  Ch=pulseToPWM(Ch);
+  return Ch;
+}
+
+
+int pulseToPWM(int pulse){
+  if (pulse>1000){
+    pulse = map(pulse, 1000, 2000, -500, 500);
+    pulse = constrain(pulse, -255, 255);  
+  }
+  else{
+    pulse=0;  
+  }
+  if(abs(pulse)<= deadzone){
+    pulse=0;
+  }
+  return pulse;
+}
+
+
+void deployment(Time, switch1_Link, switch2_Link, switch3_DoorLock, switch4_UAS) { 
+
+  //if Switch3s are not clicked (door is not unlocked), unlock door
+  if(Switch3_DoorLock == LOW) { 
+    unlockDoor(); 
+  }
+
+  //if Switch3s are clicked (door unlocked), open door
+  if(Switch3_DoorLock == HIGH) { 
+    openDoor();
+  }
+
+
+  //if Switch1_Link is unclicked and Switch2_Link is clicked (door open), deploy UAS
+  if(Switch1_Link == LOW && Switch2_Link == HIGH){ 
+    deployUAS();
+  }
+
+
+  //if Switch4_UAS is low (UAS deployed), close door
+  if(switch4_UAS == LOW) {
+    closeDoor();
+  }
+
+
+  //if Switch1_Link is clicked and Switch2_LInk is unclicked (door closed), lock door
+  if(Switch1_Link == HIGH && Switch2_Link == LOW) { 
+    lockDoor()
+    }
+ 
+}
+
+void unlockDoor() {
+  
+  for(int i = 0; i < 255; i++){ 
+    if(digitalRead(Switch3_DoorLock) == LOW || Ch4 > 200) {  
+      analogWrite(LockA_DriverIN1, i); 
+      analogWrite(LockA_DriverIN2, 0);
+      analogWrite(LockB_DriverIN1, 0); 
+      analogWrite(LockB_DriverIN2, i);
+      delay(10); 
+    }
+  }
+  
+  while(digitalRead(Switch3_DoorLock) == LOW || Ch4 > 200) { 
+    analogWrite(LockA_DriverIN1, 255); 
+    analogWrite(LockA_DriverIN2, 0);
+    analogWrite(LockB_DriverIN1, 0); 
+    analogWrite(LockB_DriverIN2, 255);
+  }
+  
+  analogWrite(LockA_DriverIN1, 0);
+  analogWrite(LockA_DriverIN2, 0);
+  analogWrite(LockB_DriverIN1, 0); 
+  analogWrite(LockB_DriverIN2, 0);
+}
+  
+  
+
+void lockDoor(lockTime) { 
+  for(int i = 0; i < 255; i++) {  
+      analogWrite(LockA_DriverIN1, 0); 
+      analogWrite(LockA_DriverIN2, i);
+      analogWrite(LockB_DriverIN1, i); 
+      analogWrite(LockB_DriverIN2, 0);
+      delay(1); 
+  }
+  
+  for(int i = 0; i < lockTime; i++) { //drives motors for lockTime seconds
+      analogWrite(LockA_DriverIN1, 0); 
+      analogWrite(LockA_DriverIN2, 255);
+      analogWrite(LockB_DriverIN1, 255); 
+      analogWrite(LockB_DriverIN2, 0);
+      delay(1000); 
+    }
+  }
+  analogWrite(LockA_DriverIN1, 0);
+  analogWrite(LockA_DriverIN2, 0);
+  analogWrite(LockB_DriverIN1, 0); 
+  analogWrite(LockB_DriverIN2, 0);
+}
+
+void openDoor() { //switches will have different states at some points so || (or) is used?????
+  for(int i = 0; i < 255; i++) { 
+    if(digitalRead(Switch1_Link) == HIGH || digitalRead(Switch2_Link) == LOW || Ch5 > 200){ 
+      analogWrite(Link_DriverIN1, i); 
+      analogWrite(Link_DriverIN2, 0);
+      delay(10); 
+    }
+  }
+  
+  while(digitalRead(Switch1_Link) == HIGH || digitalRead(Switch2_Link)|| Ch5 > 200) == LOW) { 
+    analogWrite(Link_DriverIN1, 255); 
+    analogWrite(Link_DriverIN2, 0);
+  }
+  analogWrite(Link_DriverIN1, 0); 
+  analogWrite(Link_DriverIN2, 0);
+}
+
+
+
+void closeDoor() { 
+  for(int i = 0; i < 255; i++) { 
+    if(digitalRead(Switch4_UAS) == LOW || Ch5 < 200) {
+      analogWrite(Link_DriverIN1, 0); 
+      analogWrite(Link_DriverIN2, i);
+      delay(10); 
+    }
+  }
+  
+  while(digitalRead(Switch1_Link) == LOW || digitalRead(Switch2_Link) == HIGH || Ch5 < 200) { 
+    analogWrite(Link_DriverIN1, 0); 
+    analogWrite(Link_DriverIN2, 255);
+  }
+  analogWrite(Link_DriverIN1, 0); 
+  analogWrite(Link_DriverIN2, 0);
+}
+
+
+
+void deployUAS() { 
+  for(int i = 0; i < 255; i++) { 
+    if(digitalRead(Switch1_Link) == LOW) && digitalRead(Switch2_Link) == HIGH || Ch5 > 200) { //should use and or or?
+      analogWrite(Link_DriverIN1, 0); 
+      analogWrite(Link_DriverIN2, i);
+      delay(10); 
+    }
+  }
+
+  
+  while(digitalRead(Switch1_Link) == LOW && digitalRead(Switch2_Link) == HIGH || Ch5 > 200) { 
+    analogWrite(Link_DriverIN1, 0); 
+    analogWrite(Link_DriverIN2, 255);
+  }
+  analogWrite(Link_DriverIN1, 0); 
+  analogWrite(Link_DriverIN2, 0);
+}
+
+
+void installUAS() { 
+  for(int i = 255; i < 255; i++) { 
+    analogWrite(UAS_DriverIN2, i); 
+    analogWrite(UAS_DriverIN1, 0); 
+    delay(10); 
+  }
+}
