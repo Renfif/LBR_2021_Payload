57749d79731ec650e9246e1e91eb3bf5256f6e57
Bryce Derring <31260185+Renfif@users.noreply.github.com>
2021-02-14 21:22:20 -0800

    Visual Teensy Environment For LDS

---
+++ UAS_Firmware/LDS Prototype/LDS_Prototype_Code/.vsteensy/build/lib/Adafruit_BNO055/Adafruit_BNO055.d
@@ -0,0 +1,44 @@
+.vsteensy/build/lib/Adafruit_BNO055/Adafruit_BNO055.o: \
+ C:\Users\raidr\Documents\Arduino\libraries/Adafruit_BNO055/Adafruit_BNO055.cpp \
+ C:\PROGRA~2\Arduino\hardware\teensy\avr\cores\teensy4/Arduino.h \
+ C:\PROGRA~2\Arduino\hardware\teensy\avr\cores\teensy4/WProgram.h \
+ C:\PROGRA~2\Arduino\hardware\teensy\avr\cores\teensy4/avr/pgmspace.h \
+ C:\PROGRA~2\Arduino\hardware\teensy\avr\cores\teensy4/avr/interrupt.h \
+ C:\PROGRA~2\Arduino\hardware\teensy\avr\cores\teensy4/avr_functions.h \
+ C:\PROGRA~2\Arduino\hardware\teensy\avr\cores\teensy4/wiring.h \
+ C:\PROGRA~2\Arduino\hardware\teensy\avr\cores\teensy4/binary.h \
+ C:\PROGRA~2\Arduino\hardware\teensy\avr\cores\teensy4/core_id.h \
+ C:\PROGRA~2\Arduino\hardware\teensy\avr\cores\teensy4/core_pins.h \
+ C:\PROGRA~2\Arduino\hardware\teensy\avr\cores\teensy4/imxrt.h \
+ C:\PROGRA~2\Arduino\hardware\teensy\avr\cores\teensy4/pins_arduino.h \
+ C:\PROGRA~2\Arduino\hardware\teensy\avr\cores\teensy4/HardwareSerial.h \
+ C:\PROGRA~2\Arduino\hardware\teensy\avr\cores\teensy4/Stream.h \
+ C:\PROGRA~2\Arduino\hardware\teensy\avr\cores\teensy4/Print.h \
+ C:\PROGRA~2\Arduino\hardware\teensy\avr\cores\teensy4/WString.h \
+ C:\PROGRA~2\Arduino\hardware\teensy\avr\cores\teensy4/avr/pgmspace.h \
+ C:\PROGRA~2\Arduino\hardware\teensy\avr\cores\teensy4/Printable.h \
+ C:\PROGRA~2\Arduino\hardware\teensy\avr\cores\teensy4/avr_emulation.h \
+ C:\PROGRA~2\Arduino\hardware\teensy\avr\cores\teensy4/usb_serial.h \
+ C:\PROGRA~2\Arduino\hardware\teensy\avr\cores\teensy4/usb_desc.h \
+ C:\PROGRA~2\Arduino\hardware\teensy\avr\cores\teensy4/usb_seremu.h \
+ C:\PROGRA~2\Arduino\hardware\teensy\avr\cores\teensy4/usb_keyboard.h \
+ C:\PROGRA~2\Arduino\hardware\teensy\avr\cores\teensy4/keylayouts.h \
+ C:\PROGRA~2\Arduino\hardware\teensy\avr\cores\teensy4/usb_mouse.h \
+ C:\PROGRA~2\Arduino\hardware\teensy\avr\cores\teensy4/usb_joystick.h \
+ C:\PROGRA~2\Arduino\hardware\teensy\avr\cores\teensy4/usb_midi.h \
+ C:\PROGRA~2\Arduino\hardware\teensy\avr\cores\teensy4/usb_rawhid.h \
+ C:\PROGRA~2\Arduino\hardware\teensy\avr\cores\teensy4/usb_flightsim.h \
+ C:\PROGRA~2\Arduino\hardware\teensy\avr\cores\teensy4/usb_audio.h \
+ C:\PROGRA~2\Arduino\hardware\teensy\avr\cores\teensy4/usb_touch.h \
+ C:\PROGRA~2\Arduino\hardware\teensy\avr\cores\teensy4/WCharacter.h \
+ C:\PROGRA~2\Arduino\hardware\teensy\avr\cores\teensy4/elapsedMillis.h \
+ C:\PROGRA~2\Arduino\hardware\teensy\avr\cores\teensy4/Arduino.h \
+ C:\PROGRA~2\Arduino\hardware\teensy\avr\cores\teensy4/IntervalTimer.h \
+ C:\Users\raidr\Documents\Arduino\libraries/Adafruit_BNO055/Adafruit_BNO055.h \
+ lib/Wire/Wire.h lib/Wire/WireIMXRT.h \
+ C:\Users\raidr\Documents\Arduino\libraries/Adafruit_BNO055/utility/imumaths.h \
+ C:\Users\raidr\Documents\Arduino\libraries/Adafruit_BNO055/utility/matrix.h \
+ C:\Users\raidr\Documents\Arduino\libraries/Adafruit_BNO055/utility/vector.h \
+ C:\Users\raidr\Documents\Arduino\libraries/Adafruit_BNO055/utility/quaternion.h \
+ C:\Users\raidr\Documents\Arduino\libraries/Adafruit_Unified_Sensor/Adafruit_Sensor.h \
+ C:\PROGRA~2\Arduino\hardware\teensy\avr\cores\teensy4/Print.h
---
+++ UAS_Firmware/LDS Prototype/LDS_Prototype_Code/src/main.cpp
@@ -0,0 +1,195 @@
+#include <Wire.h>
+#include <Adafruit_Sensor.h>
+#include <Adafruit_BNO055.h>
+#include <utility/imumaths.h>
+#include <AutoPID.h>
+
+//motor pins where A and B are different directions for the same motor
+#define MotorPin1A 1
+#define MotorPin1B 2
+#define MotorPin2A 3
+#define MotorPin2B 4
+#define MotorPin3A 7
+#define MotorPin3B 8
+#define MotorPin4A 5
+#define MotorPin4B 6
+//no encoders connected
+#define ReceiverPin1 23
+#define ReceiverPin2 13
+#define ReceiverPin3 14
+//pid settings and gains
+#define OUTPUT_MIN 0.0
+#define OUTPUT_MAX 255.0
+#define KP 0.1
+#define KI 0.0
+#define KD 0.0
+
+//deadzone for receiver value
+const int deadzone = 20;
+
+//PID IO and Setpoint variables
+double Input = 0;
+double Output = 0;
+double Setpoint = 0;
+
+// Set the delay between fresh samples
+uint16_t BNO055_SAMPLERATE_DELAY_MS = 100;
+//IMU object
+Adafruit_BNO055 bno = Adafruit_BNO055(55, 0x28);
+
+
+void setup() {
+  Serial.begin(115200);
+  
+  pinMode(ReceiverPin1, INPUT);
+  pinMode(ReceiverPin2, INPUT);
+  pinMode(ReceiverPin3, INPUT);
+  pinMode(MotorPin1A, OUTPUT);
+  pinMode(MotorPin1B, OUTPUT);
+  pinMode(MotorPin2A, OUTPUT);
+  pinMode(MotorPin2B, OUTPUT);
+  pinMode(MotorPin3A, OUTPUT);
+  pinMode(MotorPin3B, OUTPUT);
+  pinMode(MotorPin4A, OUTPUT);
+  pinMode(MotorPin4B, OUTPUT);
+
+  digitalWrite(MotorPin1A, LOW);
+  digitalWrite(MotorPin1B, LOW);
+  digitalWrite(MotorPin2A, LOW);
+  digitalWrite(MotorPin2B, LOW);
+  digitalWrite(MotorPin3A, LOW);
+  digitalWrite(MotorPin3B, LOW);
+  digitalWrite(MotorPin4A, LOW);
+  digitalWrite(MotorPin4B, LOW);
+  
+  if (!bno.begin())
+  {
+    /* There was a problem detecting the BNO055 ... check your connections */
+    Serial.print("Ooops, no BNO055 detected ... Check your wiring or I2C ADDR!");
+    while (1);
+  }
+  bno.setExtCrystalUse(true);
+  
+  //define I/O variables and settings!
+  AutoPID LegPID = AutoPID(&Input, &Setpoint, &Output, OUTPUT_MIN , OUTPUT_MAX, KP, KI, KD);
+  
+}
+
+void loop() {
+  //gather and print IMU data with given calibration values and delay
+  sensors_event_t orientationData;
+  bno.getEvent(&orientationData, Adafruit_BNO055::VECTOR_EULER);
+  printEvent(&orientationData);
+  uint8_t system, gyro, accel, mag = 0;
+  bno.getCalibration(&system, &gyro, &accel, &mag);
+  delay(BNO055_SAMPLERATE_DELAY_MS);
+  
+  //convert reciever input to PWM value
+  int Ch1=pulseIn(ReceiverPin1,HIGH,25000);
+  int Ch2=pulseIn(ReceiverPin2,HIGH,25000);
+  int Ch3=pulseIn(ReceiverPin3,HIGH,25000);
+  Ch1=pulseToPWM(Ch1);
+  Ch2=pulseToPWM(Ch2);
+  Ch3=pulseToPWM(Ch3);
+  //print test
+  //Serial.println("Receiver Value: ");
+  //Serial.println(Ch1);
+  
+  //leg movement cycle test
+//  while(Ch1>254){
+//    digitalWrite(MotorPin1A, HIGH);
+//    delay(3000);
+//    digitalWrite(MotorPin1A, LOW);
+//    digitalWrite(MotorPin1B, HIGH);
+//    delay(3000);
+//    digitalWrite(MotorPin1B, LOW);
+//    digitalWrite(MotorPin2A, HIGH);
+//    delay(3000);
+//    digitalWrite(MotorPin2A, LOW);
+//    digitalWrite(MotorPin2B, HIGH);
+//    delay(3000);
+//    digitalWrite(MotorPin2B, LOW);
+//    digitalWrite(MotorPin3A, HIGH);
+//    delay(3000);
+//    digitalWrite(MotorPin3A, LOW);
+//    digitalWrite(MotorPin3B, HIGH);
+//    delay(3000);
+//    digitalWrite(MotorPin3B, LOW);
+//    digitalWrite(MotorPin4A, HIGH);
+//    delay(3000);
+//    digitalWrite(MotorPin4A, LOW);
+//    digitalWrite(MotorPin4B, HIGH);
+//    delay(3000);
+//    digitalWrite(MotorPin4B, LOW);
+//    delay(5000);
+//  }
+
+  
+}
+
+int pulseToPWM(int pulse){
+  if (pulse>1000){
+    pulse = map(pulse, 1000, 2000, -500, 500);
+    pulse = constrain(pulse, -255,255);  
+  }else{
+    pulse=0;  
+  }
+  if(abs(pulse)<= deadzone){
+    pulse=0;
+  }
+  return pulse;
+}
+
+void printEvent(sensors_event_t* event) {
+  double x = -1000000, y = -1000000 , z = -1000000; //dumb values, easy to spot problem
+  if (event->type == SENSOR_TYPE_ACCELEROMETER) {
+    Serial.print("Accl:");
+    x = event->acceleration.x;
+    y = event->acceleration.y;
+    z = event->acceleration.z;
+  }
+  else if (event->type == SENSOR_TYPE_ORIENTATION) {
+    Serial.print("Orient:");
+    x = event->orientation.x;
+    y = event->orientation.y;
+    z = event->orientation.z;
+  }
+  else if (event->type == SENSOR_TYPE_MAGNETIC_FIELD) {
+    Serial.print("Mag:");
+    x = event->magnetic.x;
+    y = event->magnetic.y;
+    z = event->magnetic.z;
+  }
+  else if (event->type == SENSOR_TYPE_GYROSCOPE) {
+    Serial.print("Gyro:");
+    x = event->gyro.x;
+    y = event->gyro.y;
+    z = event->gyro.z;
+  }
+  else if (event->type == SENSOR_TYPE_ROTATION_VECTOR) {
+    Serial.print("Rot:");
+    x = event->gyro.x;
+    y = event->gyro.y;
+    z = event->gyro.z;
+  }
+  else if (event->type == SENSOR_TYPE_LINEAR_ACCELERATION) {
+    Serial.print("Linear:");
+    x = event->acceleration.x;
+    y = event->acceleration.y;
+    z = event->acceleration.z;
+  }
+  else {
+    Serial.print("Unk:");
+  }
+
+  Serial.print("\tx= ");
+  Serial.print(x);
+  Serial.print(" |\ty= ");
+  Serial.print(y);
+  Serial.print(" |\tz= ");
+  Serial.println(z);
+}
+
+void levelingWithPID(){
+    
+}
